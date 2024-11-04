044e49aa41b8fec47b985c274948efc54c7b95f4
Efrelle Joyce Ducyao <efrellejoyced@gmail.com>
2024-11-04 19:32:02 +0700

    Initial commit

---
+++ colorpreference.js
@@ -0,0 +1,13 @@
+function colors() {
+    let favcolors = [];
+
+    for (let i = 0; i < 3; i++) {
+        let enterColor = prompt("Enter Your Three Favorite colors.");
+        favcolrs.push(enterColor);
+        alert("Your Favorite Colors are: " + favcolors);
+    }
+    letpushcolor = prompt("Add another color you like.");
+    favcolors.push(pushcolor);
+    alert("Your new colors are: " + favcolors)
+
+}
